8fdfd55f0677a8a6be33950b477ea44e89d09df7
Regalis11 <poe.regalis@gmail.com>
2024-12-13 13:55:32 +0200

    Added title sceen + new monster

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public GameObject[] objectsToEnable;
+
+    private int currentIndex = 0;
+
+    public float delay;
+
+    private float timer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.anyKey || timer > delay)
+        {
+            if (currentIndex >= objectsToEnable.Length)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                objectsToEnable[currentIndex].SetActive(true);
+                currentIndex++;
+            }
+            timer = 0.0f;
+        }
+    }
+}
